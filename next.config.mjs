79bd576900c929e1b1f3397372035a11d03da92a
DannyShtep <113149172+DannyShtep@users.noreply.github.com>
2025-07-14 17:00:33 +0300

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+eslint: {
+  ignoreDuringBuilds: true,
+},
+typescript: {
+  ignoreBuildErrors: true,
+},
+images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'api.dicebear.com',
+      port: '',
+      pathname: '/7.x/avataaars/svg/**',
+    },
+  ],
+  unoptimized: true,
+},
+};
 
-export default nextConfig
+export default nextConfig;
