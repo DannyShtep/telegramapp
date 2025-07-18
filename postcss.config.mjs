e50a680d499ed506c2eb4d543425d64942ab6934
DannyShtep <113149172+DannyShtep@users.noreply.github.com>
2025-07-18 23:54:07 +0300

    Delete postcss.config.mjs

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-import tailwindcss from '@tailwindcss/postcss'; // Импортируем плагин
-import autoprefixer from 'autoprefixer'; // Импортируем autoprefixer
-
-export default {
-  plugins: {
-    tailwindcss: {}, // ИСПРАВЛЕНО: Используем объектный синтаксис для плагинов
-    autoprefixer: {},
-  },
-}
