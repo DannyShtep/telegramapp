7011d752f4f5c2c7f9acd4ac0d489b0af017e932
DannyShtep <113149172+DannyShtep@users.noreply.github.com>
2025-07-18 23:39:48 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
-import tailwindcss from '@tailwindcss/postcss'; // Импортируем новый плагин
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer'; // Убедитесь, что autoprefixer также импортируется
 
 export default {
-  plugins: {
-    [tailwindcss()]: {}, // Используем новый плагин
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss(), // Вызываем плагин как функцию
+    autoprefixer,   // Просто ссылка на импортированный autoprefixer
+  ],
 }
