d68e5bf74e0c96cded0849e66273cfffa2c59a1a
DannyShtep <113149172+DannyShtep@users.noreply.github.com>
2025-07-18 23:54:50 +0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {}, // Используем плагин Tailwind CSS для PostCSS
+    autoprefixer: {}, // Добавляем Autoprefixer
+  },
+}
