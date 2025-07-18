09d5cf7f56d6b84a2adb7ea35421ca3b3014ba2e
DannyShtep <113149172+DannyShtep@users.noreply.github.com>
2025-07-18 23:13:41 +0300

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -42,10 +42,9 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         day_hidden: "invisible",
         ...classNames,
       }}
-      // ИСПРАВЛЕНО: Возвращаем IconLeft и IconRight внутрь пропса components
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
